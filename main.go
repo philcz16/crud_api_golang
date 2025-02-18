f71b2ff0c5e135b9c3f520f94f82e9c9e65ff89f
Terpase <phil16terpasetheo@gmail.com>
2025-02-18 22:41:42 +0000

    write out add book handler logic

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
-	"os"
+	"sync"
 
-	"github.com/joho/godotenv"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -15,41 +15,85 @@ const (
 	port = 5432
 )
 
-func addBook(w http.ResponseWriter, r *http.Request) {
+type book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var (
+	books     []book
+	bookMutex sync.Mutex
+)
+
+func generateRandomId() string {
+	return uuid.New().String()
+}
+func listBooksHandler(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "Add Book page")
 }
 
-func listBooks(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "List Book page")
+func addBookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	switch r.Method {
+	case "GET":
+	case "POST":
+		var newBook book
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "unable to read from request body", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(body)
+		err = json.Unmarshal(body, &newBook)
+		if err != nil || newBook.Author == "" {
+			http.Error(w, "No Author added", http.StatusBadRequest)
+			return
+		} else if err != nil || newBook.Title == "" {
+			http.Error(w, "No Book titile added", http.StatusBadRequest)
+			return
+		}
+
+		newBook.ID = generateRandomId()
+
+		bookMutex.Lock()
+		books = append(books, newBook)
+		bookMutex.Unlock()
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newBook)
+
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+
+	}
+
 }
 
-func downloadBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Download Books page")
-}
-
-func viewBook(w http.ResponseWriter, r *http.Request) {
+func viewBookHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "View Book page")
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading env file")
-	}
-	psqlinfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	db, err := sql.Open("postgres", psqlinfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("connected to database.....")
-	http.HandleFunc("/books", listBooks)
-	http.HandleFunc("/book:book-id", viewBook)
-	http.HandleFunc("/download", downloadBook)
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading env file")
+	// }
+	// psqlinfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	// db, err := sql.Open("postgres", psqlinfo)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// _, err = db.Exec()
+	// defer db.Close()
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	http.HandleFunc("/add", addBookHandler)
+	http.HandleFunc("/books", listBooksHandler)
+	http.HandleFunc("/books/", viewBookHandler)
 	fmt.Println("server is running on Port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
